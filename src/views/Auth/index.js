aa73365b595eedaaacd42408e60382f04f3fb2c8
Samfrancisco1 <yabi.francis001@gmail.com>
2020-07-12 02:19:31 -0700

    created folder structure for dashboard

--- src/views/Auth/index.js
+++ src/views/Auth/index.js
@@ -25,66 +25,73 @@ export default class Auth extends Component {
 	render() {
 		return (
 			<>
-				<div class="auth">
-					<div class="forms-container">
-						<div class="signin-signup">
-							<form action="#" class="sign-in-form">
-								<h2 class="title">Sign in</h2>
-								<div class="input-field">
-									<i class="fas fa-user"></i>
+				<div className="auth">
+					<div className="forms-container">
+						<div className="signin-signup">
+							<form action="#" className="sign-in-form">
+								<h2 className="title">Sign in</h2>
+								<div className="input-field">
+									<i className="fas fa-user"></i>
 									<input type="text" placeholder="Username" />
 								</div>
-								<div class="input-field">
-									<i class="fas fa-lock"></i>
+								<div className="input-field">
+									<i className="fas fa-lock"></i>
 									<input type="password" placeholder="Password" />
 								</div>
-								<input type="submit" value="Login" class="btn solid" />
-	
+								<input type="submit" value="Login" className="btn solid" />
 							</form>
-							<form action="#" class="sign-up-form">
-								<h2 class="title">Sign up</h2>
-								<div class="input-field">
-									<i class="fas fa-user"></i>
+							<form action="#" className="sign-up-form">
+								<h2 className="title">Sign up</h2>
+								<div className="input-field">
+									<i className="fas fa-user"></i>
 									<input type="text" placeholder="Username" />
 								</div>
-								<div class="input-field">
-									<i class="fas fa-envelope"></i>
+								<div className="input-field">
+									<i className="fas fa-envelope"></i>
 									<input type="email" placeholder="Email" />
 								</div>
-								<div class="input-field">
-									<i class="fas fa-lock"></i>
+								<div className="input-field">
+									<i className="fas fa-lock"></i>
 									<input type="password" placeholder="Password" />
 								</div>
-								<input type="submit" class="btn" value="Sign up" />
-
+								<input type="submit" className="btn solid" value="Sign up" />
 							</form>
 						</div>
 					</div>
 
-					<div class="panels-container">
-						<div class="panel left-panel">
-							<div class="content">
+					<div className="panels-container">
+						<div className="panel left-panel">
+							<div className="content">
 								<h3>Dont have an account?</h3>
 								<p>
-									Click on the sign up button below to create and account and start earning
+									Click on the sign up button below to create and account and
+									start earning
 								</p>
-								<button onClick={ this.registerClick } class="btn transparent" id="sign-up-btn">
+								<button
+									onClick={this.registerClick}
+									class="btn transparent"
+									id="sign-up-btn"
+								>
 									Sign up
 								</button>
 							</div>
-							<img src={ login } class="image" alt="" />
+							<img src={login} class="image" alt="" />
 						</div>
-						<div class="panel right-panel">
-							<div class="content">
+						<div className="panel right-panel">
+							<div className="content">
 								<h3>Already a member?</h3>
 								<p>
 									Enter you login credentials to get access to your account.
 								</p>
-								<button onClick={ this.loginClick } class="btn transparent" id="sign-in-btn">
+								<button
+									onClick={this.loginClick}
+									className="btn transparent"
+									id="sign-in-btn"
+								>
 									Sign in
 								</button>
 							</div>
-							<img src={register} class="image" alt="" />
+							<img src={register} className="image" alt="" />
 						</div>
 					</div>
 				</div>
